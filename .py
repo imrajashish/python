d80c6989bf9d6b6c15214ddf49fa7a5568af51aa
imrajashish <imrajashish07@gmail.com>
2020-07-29 00:26:45 +0530

    second

--- .py
+++ .py
@@ -1,3 +1,4 @@
 lst=(1,2,3,4,5,6,7)
 del(lst[2])
 print(lst)
+print(lst)
--- assert.py
+++ assert.py
@@ -1,4 +1,4 @@
-x=int(input("Enter the number grster then 10"))
+x=int(input("Enter the number grster then 102"))
 assert x>10,"wrong number "
 print("u enter ",x)
 #print(x)
