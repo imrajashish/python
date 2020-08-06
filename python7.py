9a411f7a90b35ae45bcafab6377f3506f25a6718
imrajashish <imrajashish07@gmail.com>
2020-08-06 10:44:54 +0530

    number of cpus,float or interger

---
+++ python7.py
@@ -0,0 +1,8 @@
+#Write a Python program to find out the number of CPUs using.
+import multiprocessing
+print(multiprocessing.cpu_count())
+
+#Write a Python program to parse a string to Float or Integer.
+s = "2343.343"
+print(float(s))
+print(int(float(s)))
