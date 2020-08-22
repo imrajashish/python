aa758436d004ea72d980de6504f0788ce6bb2943
imrajashish <imrajashish07@gmail.com>
2020-08-22 13:04:27 +0530

    wildcard,special list,error message,+ve no for list,unicode,list of interger

--- python22.py
+++ python22.py
@@ -21,3 +21,13 @@ lst = [2,-3,4,5,-65,3]
 print("\n original number in the lst:",lst)
 new_nums = list(filter(lambda x: x>0,lst))
 print("positive number is :",new_nums)
+
+#Write a Python program to compute the product of a list of integers (without using for loop). 
+from functools import reduce
+nums = [10,20,30]
+nums_product = reduce((lambda x,y : x*y),nums)
+print("Product of the numbers : ",nums_product)
+
+#Write a Python program to print Unicode characters.
+str = u'\u0050\u0079\u0074\u0068\u006f\u006e \u0045\u0078\u0065\u0072\u0063\u0069\u0073\u0065\u0073 \u002d \u0077\u0033\u0072\u0065\u0073\u006f\u0075\u0072\u0063\u0065'
+print(str)
