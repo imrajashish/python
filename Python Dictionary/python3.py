ebaf0f2de4937424b46d3cb45b71800df7d60bec
imrajashish <imrajashish07@gmail.com>
2020-08-30 20:38:46 +0530

    three element comb,os where the script is running

---
+++ Python Dictionary/python3.py
@@ -0,0 +1,46 @@
+#Write a Python program to display some information about the OS where the script is running.
+import platform as pl
+
+os_profile = [
+        'architecture',
+        'linux_distribution',
+        'mac_ver',
+        'machine',
+        'node',
+        'platform',
+        'processor',
+        'python_build',
+        'python_compiler',
+        'python_version',
+        'release',
+        'system',
+        'uname',
+        'version',
+    ]
+for key in os_profile:
+  if hasattr(pl, key):
+    print(key +  ": " + str(getattr(pl, key)()))
+
+
+#Write a Python program to check the sum of three elements (each from an array) from three arrays is equal to a target value. Print all those three-element combinations
+import itertools
+from functools import partial
+X = [10, 20, 20, 20]
+Y = [10, 20, 30, 40]
+Z = [10, 30, 40, 20]
+T = 70
+
+def check_sum_array(N, *nums):
+  if sum(x for x in nums) == N:
+    return (True, nums)
+  else:
+      return (False, nums)
+pro = itertools.product(X,Y,Z)
+func = partial(check_sum_array, T)
+sums = list(itertools.starmap(func, pro))
+
+result = set()
+for s in sums:
+    if s[0] == True and s[1] not in result:
+      result.add(s[1])
+      print(result)
